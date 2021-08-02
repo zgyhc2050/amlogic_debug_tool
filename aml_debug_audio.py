0fa96db046d396c1fea2059ed941fcaef74bcc0e
hongchao.yin <zgyhc2050@qq.com>
2021-08-02 19:14:48 +0800

    amlogic_dump: add debug option func
    Problem:
    None
    
    Solution:
    1. add debug option func
    2. add debug time option
    
    Verify:
    Vs Code

--- aml_debug_audio.py
+++ aml_debug_audio.py
@@ -9,15 +9,23 @@ from tkinter.constants import TRUE
 DEBUG_CAPTURE_MODE_AUTO         = 0
 DEBUG_CAPTURE_MODE_MUNUAL       = 1
 DEFAULT_CAPTURE_MODE            = DEBUG_CAPTURE_MODE_AUTO
+
+class AudioDebugCfg:
+    def __init__(self):
+        self.m_debugInfoEnable = 1
+        self.m_dumpDataEnable = 1
+        self.m_logcatEnable = 1
+        self.m_printDebugEnable = 0
+
 class AmlAudioDebug:
     def __init__(self):
-        self.DUMP_DEBUG = False
-        self.AUTO_MODE_DUMP_TIME_S = 1
+        self.DUMP_DEBUG = True
+        self.DEFAULT_AUTO_MODE_DUMP_TIME_S = 3
         self.RUN_STATE_STARTED = 1
         self.RUN_STATE_STOPED = 2
         self.__curState = -1
         self.__dumpCmdOutFilePath = '/data/dump_audio.log'
-        self.__dumpCmdOutLogcatPath = '/data/logcat.txt'
+        self.__logcatFileName = 'logcat.txt'
         self.__pcRootPath = "d:\dump_audio"
         self.__adbDumpCmdLists = [
             'cat /proc/asound/cards',
@@ -41,7 +49,6 @@ class AmlAudioDebug:
             'chmod 777 /data/audio /data/vendor/audiohal/',
             'rm /data/audio/* /data/vendor/audiohal/* -rf',
             'rm ' + self.__dumpCmdOutFilePath + ' -rf',
-            'rm ' + self.__dumpCmdOutLogcatPath + ' -rf',
         ]
 
         self.__adbDumpDataStartLists = [
@@ -58,13 +65,11 @@ class AmlAudioDebug:
         ]
 
         self.__adbDumpDataStopLists = [
-            'setprop vendor.media.audio.hal.debug 0',
             'setprop vendor.media.audiohal.indump 0',
             'setprop vendor.media.audiohal.outdump 0',
             'setprop vendor.media.audiohal.alsadump 0',
             'setprop vendor.media.audiohal.a2dpdump 0',
             'setprop vendor.media.audiohal.ms12dump 0',
-            'setprop media.audio.hal.debug 0',
             'setprop media.audiohal.indump 0',
             'setprop media.audiohal.outdump 0',
             'setprop media.audiohal.alsadump 0',
@@ -72,79 +77,107 @@ class AmlAudioDebug:
             'setprop media.audiohal.ms12dump 0',
         ]
 
+        self.__adbLogcatStartLists = [
+            'setprop vendor.media.audio.hal.debug 4096',
+            'setprop media.audio.hal.debug 4096',
+        ]
+
+        self.__adbLogcatStopLists = [
+            'setprop vendor.media.audio.hal.debug 0',
+            'setprop media.audio.hal.debug 0',
+        ]
+
         self.__dumpFileLists = [
             'vendor/etc/audio_policy_configuration.xml',
             'vendor/etc/audio_policy_volumes.xml',
             '/data/audio',
             '/data/vendor/audiohal/',
-            self.__dumpCmdOutLogcatPath,
             self.__dumpCmdOutFilePath,
         ]
         self.__captureLogcatProcThread = 0
         self.__captureMode = DEFAULT_CAPTURE_MODE
         self.__nowPullPcPath = 0
         self.__callbackFunc = 0
+        self.__autoDebugTimeS = self.DEFAULT_AUTO_MODE_DUMP_TIME_S
 
     def start_capture(self, startCaptureFinish):
-        self.__callbackFunc('Start to capture the info...\n')
+        if self.__captureMode == DEBUG_CAPTURE_MODE_AUTO:
+            self.__callbackFunc('Auto mode: Start to capture the info...')
+        elif self.__captureMode == DEBUG_CAPTURE_MODE_MUNUAL:
+            self.__callbackFunc('Manual mode: Start to capture the info...')
         if self.__curState == self.RUN_STATE_STARTED:
             self.__callbackFunc('current already started, do nothing')
             return
         self.__curState = self.RUN_STATE_STARTED
-        os.system('adb root')
-        os.system('adb remount')
+        subprocess.call('adb root', shell=True)
+        subprocess.call('adb remount', shell=True)
 
         # 1. Create the audio dump directory on PC, and prepare env to debug.
         self.__nowPullPcPath = self.__capture_debug_create_directory()
         self.__prepare_debug_env()
 
-        # open the audio hal debug level for capture logcat
-        os.system('adb shell setprop vendor.media.audio.hal.debug 4096')
-        os.system('adb shell setprop media.audio.hal.debug 4096')
+        if self.__debugCfg.m_logcatEnable == 1:
+            # open the audio hal debug level for capture logcat
+            self.__capture_logcat_start()
 
         if self.__captureMode == DEBUG_CAPTURE_MODE_AUTO:
             # 2. Capture the debug info and write it to txt file.
-            self.__callbackFunc('Please wait a moment, starting to dump debugging information...')
             self.__capture_debug_text()
 
-        self.__callbackFunc('Start to capture logcat')
-        # 3. Create thread to capturing logcat
-        self.__captureLogcatProcThread = subprocess.Popen('adb shell "logcat -G 40M; logcat -c; logcat > ' + self.__dumpCmdOutLogcatPath + '"')
+        if self.__debugCfg.m_logcatEnable == 1:
+            self.__callbackFunc('3.1 Start to capture logcat')
+            # 3. Create thread to capturing logcat
+            logcat_file = open(self.__nowPullPcPath + '\\' + self.__logcatFileName, 'w')
+            subprocess.call('adb logcat -G 40M; adb logcat -c', shell=True)
+            logcmd = "adb logcat"
+            self.__captureLogcatProcThread = subprocess.Popen(logcmd,stdout=logcat_file,stderr=subprocess.PIPE)
+
+        # 4. Capture the audio data.
+        self.__capture_audio_data_start()
 
         if self.__captureMode == DEBUG_CAPTURE_MODE_AUTO:
-            # 4. Capture the audio data.
-            self.__capture_debug_dump()
-
-            self.__callbackFunc('Stop capture logcat...')
-            # 5. Kill the logcat thread, stop logcat.
-            self.__captureLogcatProcThread.kill()
+            if self.__debugCfg.m_logcatEnable == 1:
+                if self.__debugCfg.m_dumpDataEnable != 1:
+                    self.__callbackFunc('3.1 Please wait ' + str(self.DEFAULT_AUTO_MODE_DUMP_TIME_S) + 's for logcat...')
+                    time.sleep(self.__autoDebugTimeS)
+                self.__capture_logcat_stop()
+                self.__callbackFunc('3.2 Stop capture logcat...')
+                # 5. Kill the logcat thread, stop logcat.
+                self.__captureLogcatProcThread.terminate()
 
             # 6. Pull the all debug files to PC
             self.__pull_capture_debug_info_to_pc(self.__nowPullPcPath)
-
             self.__print_help_info()
             self.__curState = self.RUN_STATE_STOPED
-        elif self.__captureMode == DEBUG_CAPTURE_MODE_MUNUAL:
-            self.__callbackFunc('MUNUAL mode: Start fetching the audio data, wait for manual stop...')
-            self.__capture_debug_dump_start()
+
         startCaptureFinish()
 
     def stop_capture(self, stopcaptureFinish):
-        self.__capture_debug_dump_stop()
         if self.__curState != self.RUN_STATE_STARTED:
-            self.__callbackFunc('current no start, do nothing')
+            self.__callbackFunc('stop_capture: current no start, do nothing')
             return
-        if self.__captureLogcatProcThread != 0:
-            self.__callbackFunc('Stop capture logcat...')
-            self.__captureLogcatProcThread.kill()
-        if self.__captureMode == DEBUG_CAPTURE_MODE_MUNUAL:
-            self.__capture_debug_text()
-            self.__pull_capture_debug_info_to_pc(self.__nowPullPcPath)
-            self.__print_help_info()
-            stopcaptureFinish()
-            self.__curState = self.RUN_STATE_STOPED
-            
+        if self.__captureMode != DEBUG_CAPTURE_MODE_MUNUAL:
+            self.__callbackFunc('stop_capture: current not MUNAUL mode, not support stop!!!')
+            return
+        self.__manual_capture_stop()
+        stopcaptureFinish()
 
+    def __manual_capture_stop(self):
+        self.__callbackFunc('2.2 MUNUAL mode: fetching the audio data end.')
+        if self.__debugCfg.m_dumpDataEnable == 1:
+            self.__capture_audio_data_prop_disable()
+        if self.__debugCfg.m_logcatEnable == 1:
+            self.__callbackFunc('3.2 Stop capture logcat...')
+            self.__capture_logcat_stop()
+            if self.__captureLogcatProcThread != 0:
+                self.__captureLogcatProcThread.terminate()
+
+        if self.__debugCfg.m_debugInfoEnable == 1:
+            self.__capture_debug_text()
+        self.__pull_capture_debug_info_to_pc(self.__nowPullPcPath)
+        self.__print_help_info()
+        self.__curState = self.RUN_STATE_STOPED
+ 
     def set_capture_mode(self, mode):
         self.__captureMode = mode
 
@@ -153,6 +186,15 @@ class AmlAudioDebug:
 
     def setShowStatusCallback(self, func):
         self.__callbackFunc = func
+
+    def setAudioDebugCfg(self, cfg):
+        self.__debugCfg = cfg
+
+    def setAutoDebugTimeS(self, timeS):
+        if timeS > 999 or timeS < 0:
+            self.__callbackFunc('setAutoDebugTimeS: invalid timeS: ' + str(timeS) + ' param， use the default:' + str(self.DEFAULT_AUTO_MODE_DUMP_TIME_S) + 's')
+            timeS = self.DEFAULT_AUTO_MODE_DUMP_TIME_S
+        self.__autoDebugTimeS = timeS
 
     def __capture_debug_create_directory(self):
         currentTime = time.strftime("%Y%m%d_%H-%M-%S", time.localtime())
@@ -165,28 +207,30 @@ class AmlAudioDebug:
         return nowPullPath
 
     def __capture_debug_text(self):
-        self.__callbackFunc('Cat the some info to ' + self.__dumpCmdOutFilePath + ' file')
+        if self.__debugCfg.m_debugInfoEnable != 1:
+            return
+        self.__callbackFunc('1.1 Please wait a moment, starting to dump debugging information...')
+        self.__callbackFunc('1.2 Cat the some info to ' + self.__dumpCmdOutFilePath + ' file')
         for i, adbDumpCmdList in enumerate(self.__adbDumpCmdLists):
             echoCmdInfo = 'adb shell "echo cmd_' + str(i) + ' ' + adbDumpCmdList + ' >> ' + self.__dumpCmdOutFilePath + '"'
             exeCmdInfo = 'adb shell "' + adbDumpCmdList + ' >> ' + self.__dumpCmdOutFilePath + '"'
-            if self.DUMP_DEBUG:
-                print(echoCmdInfo)
-                print(exeCmdInfo)
-            os.system(echoCmdInfo)
-            os.system(exeCmdInfo)
+            self.__exe_adb_cmd(echoCmdInfo)
+            self.__exe_adb_cmd(exeCmdInfo)
 
-    def __capture_debug_dump(self):
-        self.__capture_debug_dump_start()
+    def __capture_audio_data_start(self):
+        if self.__debugCfg.m_dumpDataEnable != 1:
+            return
+        self.__capture_audio_data_prop_enable()
+        self.__callbackFunc('2.1 AUTO mode: Start fetching the audio data, wait for ' + str(self.__autoDebugTimeS) + ' seconds...')
         if self.__captureMode == DEBUG_CAPTURE_MODE_AUTO:
-            self.__callbackFunc('AUTO mode: Start fetching the audio data, wait for ' + str(self.AUTO_MODE_DUMP_TIME_S) + ' seconds...')
-            time.sleep(self.AUTO_MODE_DUMP_TIME_S)
-            self.__capture_debug_dump_stop()
-        elif self.__captureMode == DEBUG_CAPTURE_MODE_MUNUAL:
-            self.__callbackFunc('MUNUAL mode: Start fetching the audio data, wait for manual stop...')
+            time.sleep(self.__autoDebugTimeS)
+            self.__capture_audio_data_prop_disable()
+            self.__callbackFunc('2.2 AUTO mode: fetching the audio data end.')
 
     def __prepare_debug_env(self):
-        self.__capture_debug_dump_stop()
-        self.__capture_debug_dump_clear()
+        self.__capture_logcat_stop()
+        self.__capture_audio_data_prop_disable()
+        self.__capture_clear_all_files()
 
     def __pull_capture_debug_info_to_pc(self, pullPath):
         self.__callbackFunc('Pull all file to PC ...')
@@ -194,14 +238,20 @@ class AmlAudioDebug:
             exeCmdInfo = 'adb pull ' + dumpFile + ' ' + pullPath
             self.__exe_adb_cmd(exeCmdInfo)
 
-    def __capture_debug_dump_start(self):
+    def __capture_audio_data_prop_enable(self):
         self.__exe_adb_shell_cmd(self.__adbDumpDataStartLists)
-        
-    def __capture_debug_dump_stop(self):
+
+    def __capture_audio_data_prop_disable(self):
         self.__exe_adb_shell_cmd(self.__adbDumpDataStopLists)
 
-    def __capture_debug_dump_clear(self):
+    def __capture_clear_all_files(self):
         self.__exe_adb_shell_cmd(self.__adbDumpDataClearCmdLists)
+
+    def __capture_logcat_start(self):
+        self.__exe_adb_shell_cmd(self.__adbLogcatStartLists)
+
+    def __capture_logcat_stop(self):
+        self.__exe_adb_shell_cmd(self.__adbLogcatStopLists)
 
     def __exe_adb_shell_cmd(self, cmdLists):
         for cmd in cmdLists:
@@ -209,9 +259,9 @@ class AmlAudioDebug:
             self.__exe_adb_cmd(exeCmdInfo)
 
     def __exe_adb_cmd(self, cmd):
-        if self.DUMP_DEBUG:
-            print(cmd)
-        os.system(cmd)
+        if self.__debugCfg.m_printDebugEnable != 0:
+            self.__callbackFunc(cmd)
+        subprocess.call(cmd, shell=True)
 
     def __print_help_info(self):
             print('###############################################################################################')
--- aml_debug_ui.py
+++ aml_debug_ui.py
@@ -1,7 +1,10 @@
 import threading
 import tkinter
-import os
 import time
+import subprocess
+import tkinter as tk
+
+from tkinter import ttk
 from tkinter.constants import DISABLED, NORMAL
 from threading import Thread
 
@@ -9,53 +12,118 @@ import aml_debug_audio
 
 
 root = tkinter.Tk()
+#root.iconbitmap('tool.ico')
 root.title('Amlogic Debug Tool')
-root.wm_attributes("-alpha", 0.9)  # 设置GUI透明度(0.0~1.0)
+root.wm_attributes("-alpha", 0.95)  # 设置GUI透明度(0.0~1.0)
 root.wm_attributes("-topmost", True)  # 设置GUI置顶
-# 设定窗口的大小(长 * 宽)
-root.geometry('700x300')  
+root.geometry('650x350') # 设定窗口的大小(长 * 宽)
+root.attributes('-topmost', False)  # 窗口置顶false
 
-captureMode = tkinter.IntVar()    
-l = tkinter.Label(root, width=20, text='抓取模式')
-l.place(x=20, y=3, width=100, height=20)
+captureMode = tkinter.IntVar()
+debugInfoEnable = tkinter.IntVar()
+dumpDataEnable = tkinter.IntVar()
+logcatEnable = tkinter.IntVar()
+printDebugEnable = tkinter.IntVar()
 
-text_input = tkinter.Text(root, height=5, width=40)
-text_input.place(x=0, y=90, width=680, height=200)
+captureMode.set(aml_debug_audio.DEFAULT_CAPTURE_MODE)
+debugInfoEnable.set(1)
+dumpDataEnable.set(1)
+logcatEnable.set(1)
+printDebugEnable.set(0)
 
-scroll_text = tkinter.Scrollbar()
-#scroll_text.pack(side=tkinter.RIGHT,fill=tkinter.Y)
-scroll_text.place(x=680, y=90, width=20, height=200)
-scroll_text.config(command=text_input.yview)
+audioDebug = aml_debug_audio.AmlAudioDebug()
 
-def capture_mode_selection():
+def selection_capture_mode():
+    change_capture_mode()
+
+def clik_button_start_capture():
+    start_capture()
+
+def clik_button_stop_capture():
+    stop_capture()
+
+tabControl = ttk.Notebook(root)          # Create Tab Control
+
+tab1 = tkinter.Frame(tabControl)            # Create a tab 
+tabControl.add(tab1, text='Audio Debug')      # Add the tab
+tab2 = tkinter.Frame(tabControl)            # Add a second tab
+tabControl.add(tab2, text='Push')      # Make second tab visible
+tabControl.pack(expand=1, fill="both")  # Pack to make visible
+
+
+# LabelFrame using tab1 as the parent
+LabelFrame_DebugCaptureAudio = tkinter.LabelFrame(tab1, text=' 抓取音频数据 ')
+LabelFrame_DebugCaptureAudio.grid(row=0, column=0, rowspan=500, columnspan=800)
+
+
+LabelFrame_showInfo = tkinter.LabelFrame(LabelFrame_DebugCaptureAudio, text='cmd info:')
+LabelFrame_showInfo.grid(row=3, column=0, rowspan=700, columnspan=350, sticky='NW')
+Text_showInfo = tkinter.Text(LabelFrame_showInfo, height=13, width=88)
+Text_showInfo.grid(row=20, column=30, rowspan=300, columnspan=100)
+
+LabelFrame_captureTimeS = tkinter.LabelFrame(LabelFrame_DebugCaptureAudio, text='Auto times(s):')
+LabelFrame_captureTimeS.grid(row=0, column=3, sticky='NS')
+Text_captureTimeS = tkinter.Text(LabelFrame_captureTimeS, height=1, width=8)
+Text_captureTimeS.grid(row=1, column=3, padx=5, sticky='NS')
+Text_captureTimeS.insert("end", str(audioDebug.DEFAULT_AUTO_MODE_DUMP_TIME_S))
+
+LabelFrame_printDebug = tkinter.LabelFrame(LabelFrame_DebugCaptureAudio, text='Print Debug:')
+LabelFrame_printDebug.grid(row=1, column=3, sticky='NS')
+Checkbutton_printDebug = tkinter.Checkbutton(LabelFrame_printDebug, text='Enable', variable=printDebugEnable)
+Checkbutton_printDebug.grid(row=3, column=1)
+
+BuScrollbar_captureMode = tkinter.Scrollbar()
+#BuScrollbar_captureMode.pack(side=tkinter.RIGHT,fill=tkinter.Y)
+BuScrollbar_captureMode.place(x=627, y=140, width=20, height=200)
+BuScrollbar_captureMode.config(command=Text_showInfo.yview)
+
+# LabelFrame using tab1 as the parent
+LabelFrame_DebugCaptureAudioMode = ttk.LabelFrame(LabelFrame_DebugCaptureAudio, text=' 抓取模式: ')
+LabelFrame_DebugCaptureAudioMode.grid(row=0, column=0, rowspan=3, sticky='NW')
+Radiobutton_captureAutoMode = tkinter.Radiobutton(LabelFrame_DebugCaptureAudioMode, text='自动抓取', variable=captureMode, \
+    value=aml_debug_audio.DEBUG_CAPTURE_MODE_AUTO, command=selection_capture_mode)
+Radiobutton_captureAutoMode.grid(row=0, column=1, padx=10, pady=10)
+Radiobutton_captureManualMode = tkinter.Radiobutton(LabelFrame_DebugCaptureAudioMode, text='手动抓取', variable=captureMode, \
+    value=aml_debug_audio.DEBUG_CAPTURE_MODE_MUNUAL, command=selection_capture_mode)
+Radiobutton_captureManualMode.grid(row=1, column=1, padx=3, pady=3)
+
+Button_startCapture = tkinter.Button(LabelFrame_DebugCaptureAudio, text='开始抓取', command=clik_button_start_capture, width=10, height=2)
+Button_startCapture.grid(row=0, column=4, padx=6)
+Button_stopCapture = tkinter.Button(LabelFrame_DebugCaptureAudio, text='停止抓取', command=clik_button_stop_capture, width=10, height=2)
+Button_stopCapture.grid(row=1, column=4, padx=6, pady=9)
+
+
+LabelFrame_DebugCaptureAudioOption = ttk.LabelFrame(LabelFrame_DebugCaptureAudio, text=' Capture option: ')
+LabelFrame_DebugCaptureAudioOption.grid(row=0, column=1, rowspan=3, columnspan=1, sticky='NW')
+Checkbutton_debugInfo = tkinter.Checkbutton(LabelFrame_DebugCaptureAudioOption, text='Debug Info', variable=debugInfoEnable)
+Checkbutton_dumpData = tkinter.Checkbutton(LabelFrame_DebugCaptureAudioOption, text='Dump data', variable=dumpDataEnable)
+Checkbutton_Logcat = tkinter.Checkbutton(LabelFrame_DebugCaptureAudioOption, text='Logcat Info', variable=logcatEnable)
+Checkbutton_debugInfo.grid(row=2, column=1)
+Checkbutton_dumpData.grid(row=3, column=1)
+Checkbutton_Logcat.grid(row=4, column=1)
+
+def change_capture_mode():
     audioDebug.set_capture_mode(captureMode.get())
-    #l.config(text='you have selected ' + str(captureMode.get()))
+    if captureMode.get() == aml_debug_audio.DEBUG_CAPTURE_MODE_AUTO:
+        Text_captureTimeS.config(state=NORMAL)
+    elif captureMode.get() == aml_debug_audio.DEBUG_CAPTURE_MODE_MUNUAL:
+        Text_captureTimeS.config(state=DISABLED)
 
-captureAutoModeRadiobutton = tkinter.Radiobutton(root, text='自动抓取', variable=captureMode, value=aml_debug_audio.DEBUG_CAPTURE_MODE_AUTO, command=capture_mode_selection)
-captureAutoModeRadiobutton.place(x=20, y=30, width=100, height=20)
-captureManualModeRadiobutton = tkinter.Radiobutton(root, text='手动抓取', variable=captureMode, value=aml_debug_audio.DEBUG_CAPTURE_MODE_MUNUAL, command=capture_mode_selection)
-#captureManualModeRadiobutton.pack(side=tkinter.LEFT)
-captureManualModeRadiobutton.place(x=20, y=60, width=100, height=20)
-
-def clik_button_startCaptureInfo():
-    #text_input.delete("1.0", tkinter.END)
-    audioDebug.setShowStatusCallback(callback_showCurstatusInfo)
-    startCaptureButton.config(state=DISABLED)
-    captureAutoModeRadiobutton.config(state=DISABLED)
-    captureManualModeRadiobutton.config(state=DISABLED)
+def start_capture():
+    #Text_showInfo.delete("1.0", tkinter.END)
+    Button_startCapture.config(state=DISABLED)
+    Radiobutton_captureAutoMode.config(state=DISABLED)
+    Radiobutton_captureManualMode.config(state=DISABLED)
+    Checkbutton_debugInfo.config(state=DISABLED)
+    Checkbutton_dumpData.config(state=DISABLED)
+    Checkbutton_Logcat.config(state=DISABLED)
     startCaptureInfo()
         
-def clik_button_stopCaptureInfo():
-    stopCaptureButton.config(state=DISABLED)
+def stop_capture():
+    Button_stopCapture.config(state=DISABLED)
     stopCaptureInfo()
-    
-startCaptureButton = tkinter.Button(root, text='开始抓取', command=clik_button_startCaptureInfo)
-startCaptureButton.place(x=180, y=40, width=100, height=40)
 
-stopCaptureButton = tkinter.Button(root, text='停止抓取', command=clik_button_stopCaptureInfo)
-stopCaptureButton.place(x=350, y=40, width=100, height=40)
-
-def async_call(func):
+def __async_call(func):
     def wrapper(*args, **kwargs):
         thr = Thread(target = func, args = args, kwargs = kwargs)
         thr.start()
@@ -63,34 +131,55 @@ def async_call(func):
 
 def callback_startCaptureFinish():
     if audioDebug.get_capture_mode() == aml_debug_audio.DEBUG_CAPTURE_MODE_AUTO:
-        startCaptureButton.config(state=NORMAL)
-        captureAutoModeRadiobutton.config(state=NORMAL)
-        captureManualModeRadiobutton.config(state=NORMAL)
-        callback_showCurstatusInfo('\n######## Auto mode capture Finish !!! ############\n')
+        Button_startCapture.config(state=NORMAL)
+        Radiobutton_captureAutoMode.config(state=NORMAL)
+        Radiobutton_captureManualMode.config(state=NORMAL)
+        Checkbutton_debugInfo.config(state=NORMAL)
+        Checkbutton_dumpData.config(state=NORMAL)
+        Checkbutton_Logcat.config(state=NORMAL)
+        callback_showCurstatusInfo('\n######## Auto mode capture Finish !!! ############')
     elif audioDebug.get_capture_mode() == aml_debug_audio.DEBUG_CAPTURE_MODE_MUNUAL:
-        stopCaptureButton.config(state=NORMAL)
+        Button_stopCapture.config(state=NORMAL)
+        callback_showCurstatusInfo('Manual mode Start capture finish')
 
 def callback_stopCaptureFinish():
-    callback_showCurstatusInfo('\n######## Munual mode capture Finish !!! ############\n')
-    startCaptureButton.config(state=NORMAL)
-    captureAutoModeRadiobutton.config(state=NORMAL)
-    captureManualModeRadiobutton.config(state=NORMAL)
+    callback_showCurstatusInfo('\n######## Manual mode capture Finish !!! ############')
+    Button_startCapture.config(state=NORMAL)
+    Radiobutton_captureAutoMode.config(state=NORMAL)
+    Radiobutton_captureManualMode.config(state=NORMAL)
+    Checkbutton_debugInfo.config(state=NORMAL)
+    Checkbutton_dumpData.config(state=NORMAL)
+    Checkbutton_Logcat.config(state=NORMAL)
 
-@async_call
+@__async_call
 def startCaptureInfo():
+    audioDebugConfig = aml_debug_audio.AudioDebugCfg()
+    audioDebugConfig.m_debugInfoEnable = debugInfoEnable.get()
+    audioDebugConfig.m_dumpDataEnable = dumpDataEnable.get()
+    audioDebugConfig.m_logcatEnable = logcatEnable.get()
+    audioDebugConfig.m_printDebugEnable = printDebugEnable.get
+    audioDebug.setAudioDebugCfg(audioDebugConfig)
+    audioDebug.setShowStatusCallback(callback_showCurstatusInfo)
+
+    Text_captureTimeS_value = str(Text_captureTimeS.get(1.0, tkinter.END))
+    try:
+        captureTimeS = int(Text_captureTimeS_value)
+    except ValueError:
+        captureTimeS = audioDebug.DEFAULT_AUTO_MODE_DUMP_TIME_S
+        callback_showCurstatusInfo('startCaptureInfo: invalid captureTimeS!!!')
+    audioDebug.setAutoDebugTimeS(captureTimeS)
+
     audioDebug.start_capture(callback_startCaptureFinish)
 
-@async_call
+
+@__async_call
 def stopCaptureInfo():
     audioDebug.stop_capture(callback_stopCaptureFinish)
 
-captureMode.set(aml_debug_audio.DEFAULT_CAPTURE_MODE)
-stopCaptureButton.config(state=DISABLED)
-
 def callback_showCurstatusInfo(infoText):
-    text_input.mark_set("here", "0.0")
-    text_input.insert("here", infoText + ' \n')
+    Text_showInfo.mark_set("here", "0.0")
+    Text_showInfo.insert("here", infoText + ' \n')
 
-audioDebug = aml_debug_audio.AmlAudioDebug()
+Button_stopCapture.config(state=DISABLED)
 
 root.mainloop()
